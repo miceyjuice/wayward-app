75a3dc9ef0c513bf4d436411ca7cca3a576d803d
Pawel Szumiec <pawel.szumiec98@gmail.com>
2022-03-04 21:46:42 +0100

    MongoDb connected

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "../backend/routes/userRoutes.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+import { connectDb } from "./config/db.js";
 
 dotenv.config();
+
+connectDb();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: false }))
 
 app.use("/api/users", userRoutes);
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server is listening on port ${port}`));
